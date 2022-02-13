6a20a6cccab64a92e9e00631a3df5caa3f2e8927
aliaaosama23 <engaliaaosama@gmail.com>
2022-02-13 04:10:17 +0200

    move apikey to constants file

--- lib/services/currency_service.dart
+++ lib/services/currency_service.dart
@@ -1,36 +1,12 @@
-// coin api
-// API Key: A6DAE56F-9B8C-4B06-B366-791024DA9358
-
-// end points
-// https://rest.coinapi.io/v1/exchangerate/asset_id_base/asset_id_quote
-// https://rest.coinapi.io/v1/exchangerate/BTC/USD
-// https://rest.coinapi.io/v1/exchangerate/BTC
-//https://rest.coinapi.io/v1/exchangerate/BTC/EUR?
-// apikey=A6DAE56F-9B8C-4B06-B366-791024DA9358
-// &time=2022-02-12T01:18:33.2000000Z
 import 'dart:convert';
 
 import 'package:currency_converter/models/currency_rate.dart';
+import 'package:currency_converter/utilites/constants.dart';
 import 'package:intl/intl.dart';
 import 'package:http/http.dart' as http;
 
-const kAPIKEY = 'A6DAE56F-9B8C-4B06-B366-791024DA9358';
-
 class CurrencyService {
   static String formattedDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
-
-  // Future<CurrencyRate> fetchCurrencyExchangeRate() async {
-  //   final response = await http.get(Uri.parse(
-  //       'https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=$kAPIKEY&time=$formattedDate'));
-  //
-  //   if (response.statusCode == 200) {
-  //     print('returned body is ${response.body}');
-  //     print('decoded json ${jsonDecode(response.body)}');
-  //     return CurrencyRate.fromJson(jsonDecode(response.body));
-  //   } else {
-  //     throw Exception('Failed to load rate data');
-  //   }
-  // }
 
   Future<CurrencyRate> fetchCurrencyExchangeRateDependOnSelectedCurrency(
       String currencyBase, String currencyQuote) async {
--- lib/utilites/constants.dart
+++ lib/utilites/constants.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+const kAPIKEY = 'A6DAE56F-9B8C-4B06-B366-791024DA9358';
+
 const kCurrencyStyle = TextStyle(
   color: Colors.white,
   fontSize: 25,
